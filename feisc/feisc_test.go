d9e4c527658e234f7048a4fbdfec813dd0dd4213
fboyer <fboyer@gmail.com>
2013-11-27 17:29:42 -0500

    Added/Updated tests

---
+++ feisc/feisc_test.go
@@ -0,0 +1,5 @@
+package feisc
+
+import (
+	"testing"
+)
--- reader/reader_test.go
+++ reader/reader_test.go
@@ -6,9 +6,11 @@ import (
 
 func TestConnect(t *testing.T) {
 	const hostAddr = "192.168.23.90"
-	// To-Do
+	if socketHnd, _ := Connect(hostAddr, 4001); socketHnd < 0 {
+		t.Errorf("Connect(%v) = %v", hostAddr, socketHnd)
+	}
 }
 
-func TestDisconnect(t *testing.T) {
-	// To-Do
-}
+// func TestDisconnect(t *testing.T) {
+// To-Do
+// }
