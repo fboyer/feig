ef0c3ca2a20b1e55ecd634b87006ae630bc46088
fboyer <fboyer@gmail.com>
2013-11-28 18:33:41 -0500

    Corrected current tests to match the new Reader structure

--- reader/reader_test.go
+++ reader/reader_test.go
@@ -8,12 +8,16 @@ var (
 	r = Reader{hostAddr: "192.168.20.112", port: 4001, busAddr: 255}
 )
 
-func TestReaderConnect(t *testing.T) {
-	if err := Connect(hostAddr, 4001); err != nil {
-		t.Errorf("Connect(%v) = %v", r.hostAddr, r.socketHnd)
+func TestConnect(t *testing.T) {
+	if err := r.Connect(); err != nil {
+		t.Errorf("r.Connect() = %v", r.socketHnd)
 	}
+	r.Disconnect()
 }
 
-// func TestDisconnect(t *testing.T) {
-// To-Do
-// }
+func TestDisconnect(t *testing.T) {
+	r.Connect()
+	if status, err := r.Disconnect(); err != nil {
+		t.Errorf("r.Disconnect() = %v", status)
+	}
+}
